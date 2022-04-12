165192fb1d2ffc52a6cb8b144435851ff94a6348
lucngassa <51915152+lucngassa@users.noreply.github.com>
2022-04-12 13:22:43 -0400

    Update instance.tf

--- workstation_linux/instance.tf
+++ workstation_linux/instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "workstation" {
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
   # the public SSH key
-  key_name = aws_key_pair.mykeypair.key_name
+  key_name = aws_key_pair.Docker_key.key_name
   user_data = local.template_file_int
 
   tags = {
